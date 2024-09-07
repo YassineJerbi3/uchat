60901d8a185f92a4ef4016fb07142a06ebdae702
Yassine Jerbi <jerbiyassine536@gmail.com>
2024-09-07 03:24:30 +0100

    first uodate

---
+++ android/app/src/main/kotlin/com/example/uchat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'firebase_options.dart'; // Import the generated file
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Example'),
+      ),
+      body: const Center(
+        child: Text('Hello, Firebase!'),
+      ),
+    );
+  }
+}
